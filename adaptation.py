1a18d108efbc77961e66857c1d063ac2c6faa653
root <nicolasjonason@gmail.com>
2022-03-15 10:08:52 +0000

    fix bugs in comparison experiment

--- adaptation.py
+++ adaptation.py
@@ -31,11 +31,11 @@ def window_sample(instance,win_s,hop_s):
     return instance
 
 def join_and_window(instance,win_s=4,hop_s=1):
+    instance=copy.deepcopy(instance)
     return window_sample(join_batch(instance),win_s,hop_s)
 
 def rf2cf(row_form):
     return {k:[s[k] for s in row_form] for k in row_form[0].keys()}
-
 
 def playback_and_save(x,fn,DEMO_PATH):
     print(fn)
@@ -44,9 +44,7 @@ def playback_and_save(x,fn,DEMO_PATH):
     path=DEMO_PATH+f"{fn}.wav"
     soundfile.write(path,x,SAMPLE_RATE)
 
-
 def stitch(audios):
-
   RENDER_OVERLAP_S=1
   out=np.zeros(N_SAMPLES*len(audios))
   tail_taper= np.concatenate([np.ones(RENDER_OVERLAP_S*SAMPLE_RATE),np.ones(N_SAMPLES-(RENDER_OVERLAP_S*SAMPLE_RATE*2)),np.linspace(1,0,RENDER_OVERLAP_S*SAMPLE_RATE)])
@@ -76,7 +74,6 @@ def render_example(dataset,model, transform_key=None,transform_fn=lambda x:x):
 # LOAD MODEL FOR FINETUNING
 
 def get_finetuning_model(full_ir_duration,free_ir_duration,checkpoint_path):
-
     # load model
     test_model=shared_model.get_model(SAMPLE_RATE,CLIP_S,FT_FRAME_RATE,Z_SIZE,N_INSTRUMENTS,IR_DURATION,BIDIRECTIONAL,USE_F0_CONFIDENCE,N_HARMONICS,N_NOISE_MAGNITUDES,losses=[])
     # load model weights       
@@ -144,6 +141,14 @@ def get_finetuning_model(full_ir_duration,free_ir_duration,checkpoint_path):
 # %%
 # HP TUNING ON OTHER SET?
 
+# constants
+
+TRAIN_DATA_DURATIONS = [4*(2**i) for i in range(6)]
+#TRAIN_DATA_DURATIONS=[16]
+BATCH_SIZE=1
+DEMO_IR_DURATION=1
+MAX_DISPLAY_SECONDS=32
+
 # comparison tst
 trn_data_provider=data.MultiTFRecordProvider(f"datasets/comparison_experiment/tfr/dev/*",sample_rate=SAMPLE_RATE)
 trn_dataset= trn_data_provider.get_dataset(shuffle=False)
@@ -151,25 +156,20 @@ trn_dataset= trn_data_provider.get_dataset(shuffle=False)
 tst_data_provider=data.MultiTFRecordProvider(f"datasets/comparison_experiment/tfr/tst/*",sample_rate=SAMPLE_RATE)
 tst_dataset=tst_data_provider.get_dataset(shuffle=False)
 
-# constants
-
-BATCH_SIZE=1
-DEMO_IR_DURATION=1
+tst_data_display=next(iter(tst_dataset.take(MAX_DISPLAY_SECONDS//CLIP_S).batch(MAX_DISPLAY_SECONDS//CLIP_S)))
+tst_data_display_wd=tf.data.Dataset.from_tensor_slices(join_and_window(tst_data_display,4,3)).batch(BATCH_SIZE)
 
 # set adaptation strategy
-pretrained_checkpoint_path="./artefacts/training/Saxophone/ckpt-250000"
+pretrained_checkpoint_path="./artefacts/training/Saxophone/ckpt-300000"
 finetune_whole=True
 free_ir_duration=0.2
 ir_duration=1
 
-TRAIN_DATA_DURATIONS = [4*(2**i) for i in range(6)]
-#TRAIN_DATA_DURATIONS=[16]
-
-tst_model=get_finetuning_model(ir_duration,free_ir_duration,pretrained_checkpoint_path)
+model=get_finetuning_model(ir_duration,free_ir_duration,pretrained_checkpoint_path)
 
 # set learning rate and n epochs based on adaptation strategy
 if pretrained_checkpoint_path!=None:
-    tst_model.set_is_shared_trainable(finetune_whole)
+    model.set_is_shared_trainable(finetune_whole)
     if finetune_whole:
         lr=3e-5
         n_iterations=1000
@@ -177,24 +177,28 @@ if pretrained_checkpoint_path!=None:
         lr=1e-3
         n_iterations=1000
 else:
-    tst_model.set_is_shared_trainable(True)
+    model.set_is_shared_trainable(True)
     lr=1e-4
     n_iterations=1000
 
 for train_data_duration in TRAIN_DATA_DURATIONS:
-
-    OUTPUT_PATH=f"artefacts/comparison_experiment/{pretrained_checkpoint_path}_trn_data_duration={train_data_duration}_finetunewhole={finetune_whole}_free_ir={free_ir_duration}/"
+    OUTPUT_PATH=f"comparison_experiment/{pretrained_checkpoint_path}_trn_data_duration={train_data_duration}_finetunewhole={finetune_whole}_free_ir={free_ir_duration}/"
 
     trn_log_dir = OUTPUT_PATH + '/trn'
     tst_log_dir = OUTPUT_PATH + '/tst'
     trn_summary_writer = tf.summary.create_file_writer(trn_log_dir)
     tst_summary_writer = tf.summary.create_file_writer(tst_log_dir)
 
-    trn_clips=len(trn_dataset)
+    trn_clips=train_data_duration//CLIP_S
     # load correct amount of training data and window it
     trn_data=next(iter(trn_dataset.take(trn_clips).batch(trn_clips)))
     trn_data_batched=tf.data.Dataset.from_tensor_slices(join_and_window(trn_data,4,1)).batch(BATCH_SIZE)
-    n_batches=len(trn_data_batched)
+    n_batches=len(list(trn_data_batched))
+
+    trn_data_display=next(iter(trn_dataset.take(min(MAX_DISPLAY_SECONDS,train_data_duration)//CLIP_S).batch(min(MAX_DISPLAY_SECONDS,train_data_duration)//CLIP_S)))
+    trn_data_display_wd=tf.data.Dataset.from_tensor_slices(join_and_window(trn_data_display,4,3)).batch(BATCH_SIZE)
+
+    summary_interval = 20/(n_batches*BATCH_SIZE)
 
     # batch tst data
     tst_data_batched=tst_dataset.batch(BATCH_SIZE)
@@ -202,77 +206,75 @@ for train_data_duration in TRAIN_DATA_DURATIONS:
     # set up optimizer
     optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
 
-    epoch_count=0
     trn_losses=[]
     tst_losses=[]
 
-    n_epochs=n_iterations//(BATCH_SIZE*n_batches)
+    epoch_count=0
+
+    n_epochs=n_iterations//(n_batches*BATCH_SIZE)
 
     while epoch_count<n_epochs:
-
         print(f"{epoch_count}/{n_epochs}")
-
         trn_data_batched=trn_data_batched.shuffle(100000)
 
         epoch_loss=0
         batch_counter=0
-        
-        tst_epoch_loss=0
-        tst_batch_counter=0
 
         for trn_batch in trn_data_batched:
             with tf.GradientTape() as tape:
-                output = tst_model(trn_batch)
+                output = model(trn_batch)
                 loss_value=spectral_loss(trn_batch["audio"],output["audio_synth"])
                 epoch_loss+=loss_value.numpy()
                 batch_counter+=1
-                gradients = tape.gradient(loss_value, tst_model.trainable_weights)
-            optimizer.apply_gradients(zip(gradients, tst_model.trainable_weights))
+                gradients = tape.gradient(loss_value, model.trainable_weights)
+            optimizer.apply_gradients(zip(gradients, model.trainable_weights))
         trn_losses.append(epoch_loss/batch_counter)
 
-        for tst_batch in tst_data_batched:
-            tst_output=tst_model(tst_batch)
-            loss_value=spectral_loss(tst_batch["audio"],tst_output["audio_synth"])   
-            tst_epoch_loss+=loss_value.numpy()
-            tst_batch_counter+=1
-        tst_losses.append(tst_epoch_loss/tst_batch_counter)
-
         with trn_summary_writer.as_default():
-            tf.summary.scalar('loss', epoch_loss/batch_counter, step=epoch_count*BATCH_SIZE*n_batches)
-        with tst_summary_writer.as_default():
-            tf.summary.scalar('loss', tst_epoch_loss/tst_batch_counter, step=epoch_count*BATCH_SIZE*n_batches)
+            tf.summary.scalar('loss', epoch_loss/batch_counter, step=epoch_count*n_batches*BATCH_SIZE)
+            tf.summary.audio("target",tf.reshape(trn_batch["audio"],[-1])[None,None,...],SAMPLE_RATE,step=epoch_count*n_batches*BATCH_SIZE)
+            tf.summary.audio("estimate",tf.reshape(output["audio_synth"],[-1])[None,None,...],SAMPLE_RATE,step=epoch_count*n_batches*BATCH_SIZE)
 
+        if epoch_count%summary_interval==0:
+
+            with trn_summary_writer.as_default():
+                tf.summary.audio("target",tf.reshape(trn_batch["audio"],[-1])[None,None,...],SAMPLE_RATE,step=epoch_count*n_batches*BATCH_SIZE)
+                tf.summary.audio("estimate",tf.reshape(output["audio_synth"],[-1])[None,None,...],SAMPLE_RATE,step=epoch_count*n_batches*BATCH_SIZE)
+
+            tst_epoch_loss=0
+            tst_batch_counter=0
+            for tst_batch in tst_data_batched:
+                tst_output=model(tst_batch)
+                loss_value=spectral_loss(tst_batch["audio"],tst_output["audio_synth"])   
+                tst_epoch_loss+=loss_value.numpy()
+                tst_batch_counter+=1
+            tst_losses.append(tst_epoch_loss/tst_batch_counter)
+            with tst_summary_writer.as_default():
+                tf.summary.scalar('loss', tst_epoch_loss/tst_batch_counter, step=epoch_count*n_batches*BATCH_SIZE)
+                tf.summary.audio("target",tf.reshape(tst_batch["audio"],[-1])[None,None,...],SAMPLE_RATE,step=epoch_count*n_batches*BATCH_SIZE)
+                tf.summary.audio("estimate",tf.reshape(tst_output["audio_synth"],[-1])[None,None,...],SAMPLE_RATE,step=epoch_count*n_batches*BATCH_SIZE)
         epoch_count+=1
 
-    # write losses to csv
-    pd.DataFrame({"train_loss":trn_losses,"tst_loss":tst_losses}).to_csv(OUTPUT_PATH+"losses.csv")
-
     # RENDER AUDIO EXAMPLES
-    playback_and_save(tf.reshape(trn_data["audio"],[-1]),"training data",OUTPUT_PATH)
-
-    # transform data so that the clips overlap
-    trn_data_display=tf.data.Dataset.from_tensor_slices(join_and_window(trn_data,4,3)).batch(BATCH_SIZE)
-
+    playback_and_save(tf.reshape(trn_data_display["audio"],[-1]),"training data",OUTPUT_PATH)
+    
     # First trn data
-    playback_and_save(render_example(trn_data_display,tst_model),"training estimate",OUTPUT_PATH)
-    playback_and_save(render_example(trn_data_display,tst_model,"f0_hz",lambda x:x*(3/4)),"transposed down a fourth",OUTPUT_PATH)
-    playback_and_save(render_example(trn_data_display,tst_model,"f0_hz",lambda x:x*(4/3)),"transposed up a fourth",OUTPUT_PATH)
-    playback_and_save(render_example(trn_data_display,tst_model,"loudness_db",lambda x:x-12),"loudness down 12 db",OUTPUT_PATH)
-    playback_and_save(render_example(trn_data_display,tst_model,"loudness_db",lambda x:x-6),"loudness down 6 db",OUTPUT_PATH)
-    playback_and_save(render_example(trn_data_display,tst_model,"loudness_db",lambda x:x+6),"loudness up 6 db",OUTPUT_PATH)
-    playback_and_save(render_example(trn_data_display,tst_model,"loudness_db",lambda x:x+12),"loudness up 12 db",OUTPUT_PATH)
-    playback_and_save(render_example(trn_data_display,tst_model,"f0_confidence",lambda x:x*0.8),"pitch confidence * 0.8",OUTPUT_PATH)
-    playback_and_save(render_example(trn_data_display,tst_model,"f0_confidence",lambda x:x*0.5),"pitch confidence * 0.5",OUTPUT_PATH)
+    playback_and_save(render_example(trn_data_display,model),"training estimate",OUTPUT_PATH)
+    playback_and_save(render_example(trn_data_display,model,"f0_hz",lambda x:x*(3/4)),"transposed down a fourth",OUTPUT_PATH)
+    playback_and_save(render_example(trn_data_display,model,"f0_hz",lambda x:x*(4/3)),"transposed up a fourth",OUTPUT_PATH)
+    playback_and_save(render_example(trn_data_display,model,"loudness_db",lambda x:x-12),"loudness down 12 db",OUTPUT_PATH)
+    playback_and_save(render_example(trn_data_display,model,"loudness_db",lambda x:x-6),"loudness down 6 db",OUTPUT_PATH)
+    playback_and_save(render_example(trn_data_display,model,"loudness_db",lambda x:x+6),"loudness up 6 db",OUTPUT_PATH)
+    playback_and_save(render_example(trn_data_display,model,"loudness_db",lambda x:x+12),"loudness up 12 db",OUTPUT_PATH)
+    playback_and_save(render_example(trn_data_display,model,"f0_confidence",lambda x:x*0.8),"pitch confidence * 0.8",OUTPUT_PATH)
+    playback_and_save(render_example(trn_data_display,model,"f0_confidence",lambda x:x*0.5),"pitch confidence * 0.5",OUTPUT_PATH)
 
     # save tst data
-    playback_and_save(tf.reshape(tst_data["audio"],[-1]),"unseen data",OUTPUT_PATH)
-
+    playback_and_save(tf.reshape(tst_data_display["audio"],[-1]),"unseen target",OUTPUT_PATH)
      # transform data so that the clips overlap
-    tst_data_display=tf.data.Dataset.from_tensor_slices(join_and_window(tst_data,4,3)).batch(BATCH_SIZE)
-
-    playback_and_save(render_example(tst_batched,tst_model),"unseen estimate",OUTPUT_PATH)
+    playback_and_save(render_example(tst_data_display_wd,model),"unseen estimate",OUTPUT_PATH)
     
-
+asd
 # %%
 # render other examples
 
